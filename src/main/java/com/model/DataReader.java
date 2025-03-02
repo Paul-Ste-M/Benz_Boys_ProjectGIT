b90f5d039237308da221b2c8602c445686c01613
Paul-Ste-M <internandscholarpaul1@gmail.com>
2025-03-02 11:42:29 -0500

    Added DataReader Stub

---
+++ src/main/java/com/model/DataReader.java
@@ -0,0 +1,18 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class DataReader {
+    public ArrayList<Instrument> getSounds(){
+            return null;
+
+    }
+    public ArrayList<Song> getSongs(){
+            return null;
+
+    }
+    public ArrayList<User> getUsers(){
+            return null;
+        
+    }
+}
