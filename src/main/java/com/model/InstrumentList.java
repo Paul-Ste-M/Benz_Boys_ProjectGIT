2b08e7cb44b9a248269ce87180c8c7f20e456612
Paul-Ste-M <internandscholarpaul1@gmail.com>
2025-03-02 11:36:52 -0500

    Added InstrumentList Stub

---
+++ src/main/java/com/model/InstrumentList.java
@@ -0,0 +1,19 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class InstrumentList {
+    private ArrayList<Instrument> Instruments;
+
+    public void addInstument(Instrument Instrument){
+
+    }
+    public void removeiInstrument(Instrument Instrument){
+
+    }
+    public ArrayList<Instrument> getInstruments(){
+            return Instruments;
+        
+    }
+}
+
