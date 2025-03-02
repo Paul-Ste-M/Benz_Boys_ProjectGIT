5bec869c9d3c9b16aa1a7a097948a4195d6722c2
Paul-Ste-M <internandscholarpaul1@gmail.com>
2025-03-02 11:22:05 -0500

    Added EarTrainingGame

---
+++ src/main/java/com/model/EarTrainingGame.java
@@ -0,0 +1,39 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class EarTrainingGame {
+    private Chord currentCorrectChord;
+    private ArrayList<Chord> options;
+    private static final int NUM_ROUNDS = 5;
+
+    public EarTrainingGame() {
+        // Constructor: Initialize any necessary values
+    }
+
+    public void playChord() {
+        // Logic to play a chord
+    }
+
+    public void displayOptions() {
+        // Logic to display possible chord options to the user
+    }
+
+    public void generateOptions() {
+        // Logic to generate possible answer choices
+    }
+
+    public void chooseNewChord() {
+        // Logic to randomly select a new chord
+    }
+
+    public void nextRound() {
+        // Logic to move to the next round
+    }
+
+    public boolean checkAnswer(String userDecision) {
+        // Logic to verify if the user's answer is correct
+        return false; // Placeholder return value
+    }
+}
+
