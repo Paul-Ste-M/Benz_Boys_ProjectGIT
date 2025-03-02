fdb12df083526d0864ddbbd43760d18505ddbf7b
Paul-Ste-M <internandscholarpaul1@gmail.com>
2025-03-02 11:47:39 -0500

    Added DataWriter Stub

---
+++ src/main/java/com/model/DataWriter.java
@@ -0,0 +1,17 @@
+package com.model;
+import java.util.ArrayList;
+public class DataWriter {
+
+    public boolean saveUsers(ArrayList<User> users) {
+            return false;
+    }
+
+    public boolean saveSongs(ArrayList<Song> songs) {
+            return false;
+    }
+
+    public boolean saveSounds(ArrayList<Instrument> sounds) {
+        return true;
+    }
+}
+
