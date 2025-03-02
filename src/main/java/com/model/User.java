322b351c014029fd7f828b195e34db474bd60b98
Paul-Ste-M <internandscholarpaul1@gmail.com>
2025-03-02 10:47:20 -0500

    Added User Stub

---
+++ src/main/java/com/model/User.java
@@ -0,0 +1,36 @@
+package com.model;
+
+import java.util.UUID;
+
+public class User {
+    
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String email;
+    private UUID userID;
+
+public User(String firstName, String lastName, String userName,  String password, String email){
+}
+public void addSong(song: Song){
+
+}
+public void  removeSong(song: Song){
+}
+public boolean Author(String firstName, String lastName, String userName, String password, String email){
+
+}
+public void playSong(Song Song){
+    
+}
+public void pauseSong(Song song){
+    
+}
+public void volumeIncrease(){
+    
+}
+public void volumeDescrease(){
+    
+}
+}
