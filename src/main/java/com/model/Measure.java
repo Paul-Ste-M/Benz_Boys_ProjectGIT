e0c1c6a88b6db8ba809b003c1e7cc5903bd6dbec
Paul-Ste-M <internandscholarpaul1@gmail.com>
2025-03-02 11:11:12 -0500

    Added Measure Stub

---
+++ src/main/java/com/model/Measure.java
@@ -0,0 +1,27 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class Measure {
+    private ArrayList<Chord> chords;
+    private int beatCount;
+    
+    public Measure() {
+    }
+    
+    public boolean checkIfValid() {
+        // Implementation for checking if the measure is valid
+        return true;
+    }
+    
+    public void addChord(String type, String pitch, int position) {
+
+    }
+    
+    public void removeChord(int position) {
+    }
+    
+    public void changeChord(String type, String pitch, int position) {
+    }
+}
+
