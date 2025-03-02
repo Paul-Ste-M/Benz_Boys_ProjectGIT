405535281e687f1e1081cc66881cf4b4c452d23e
Paul-Ste-M <internandscholarpaul1@gmail.com>
2025-03-02 10:41:19 -0500

    Added UserList Stub

---
+++ src/main/java/com/model/UserList.java
@@ -0,0 +1,27 @@
+package com.model;
+
+public class UserList {
+    private Arraylist<User> Users;
+    private UserList userList;
+
+
+    private UserList(){
+    }
+
+    public UserList getInstance(){
+    }
+
+    public void addUser (String fistName, String lastName. String, password, Int User UUID, String eail){
+
+    }
+    public User getUser(String userName, String password){
+    }
+    public void removeUser(User user){
+
+    }
+    public void displayUsers(){
+    }
+    public boolean saveUsers(){
+        return true;
+    }
+}
