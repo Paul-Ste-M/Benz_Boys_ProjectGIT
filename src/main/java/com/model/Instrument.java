03acecf256b0be669af992a77b46da237d7f668b
Paul-Ste-M <internandscholarpaul1@gmail.com>
2025-03-02 11:33:07 -0500

    Added Instrument Stub and InstrumentType Enum

---
+++ src/main/java/com/model/Instrument.java
@@ -0,0 +1,16 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class Instrument {
+    private static final String INSTTUMENT_SOUND_FILE = "";
+    private InstrumentType Instrument;
+    private ArrayList<String> chords;
+
+    public void playSound(String pitch){
+    }
+
+    public void loadinSounds(){
+        
+    }
+}
