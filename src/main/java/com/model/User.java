898fbc59d0f9badde9f8bfa1ff9179107d86e132
Paul-Ste-M <internandscholarpaul1@gmail.com>
2025-03-04 13:54:36 -0500

    Added Getters and Setters

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -11,26 +11,45 @@ public class User {
     private String email;
     private UUID userID;
 
-public User(String firstName, String lastName, String userName,  String password, String email){
+public User(String firstName, String lastName, String userName,  String password, String email, UUID userID){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.userName = userName;
+    this.password = password;
+    this.email = email;
 }
-public void addSong(song: Song){
 
+public String getfirstName() {
+    return firstName;
 }
-public void  removeSong(song: Song){
+public String getlastName() {
+    return lastName;
 }
-public boolean Author(String firstName, String lastName, String userName, String password, String email){
-
+public String getUsername() {
+    return userName;
+}
+public String getpassword() {
+    return password;
+}
+public String getemail(){
+    return email; 
+}
+public UUID userID(){
+    return userID;
+}
+public boolean Author(String firstName, String lastName, String userName, String password, String email, UUID userID){
+    return true;
+}
+public void addSong(Song Song){
+}
+public void removeSong(Song Song){
 }
 public void playSong(Song Song){
-    
 }
 public void pauseSong(Song song){
-    
 }
-public void volumeIncrease(){
-    
+public void volumeIncrease(){   
 }
-public void volumeDescrease(){
-    
+public void volumeDescrease(){   
 }
 }
