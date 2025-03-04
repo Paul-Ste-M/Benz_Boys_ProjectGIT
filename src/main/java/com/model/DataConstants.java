50bab86011e8210a137d7aa2799946a867311d65
JCantave431 <jenterra10@gmail.com>
2025-03-04 14:21:19 -0500

    #26 added DataConstants.java and the strings for User.java's attributes

---
+++ src/main/java/com/model/DataConstants.java
@@ -0,0 +1,12 @@
+package com.model;
+
+public abstract class DataConstants {
+    protected static final String USER_FILE_NAME = "json/users.json";
+    protected static final String USER_TEMP_FILE_NAME = "json/users_temp.json";
+    protected static final String USER_FIRST_NAME = "firstName";
+    protected static final String USER_LAST_NAME = "lastName";
+    protected static final String USER_PASSWORD = "password";
+    protected static final String USER_EMAIL = "email";
+    protected static final String USER_ID = "userID";
+    protected static final String USER_CREATED_SONGS = "createdSongs";
+}
